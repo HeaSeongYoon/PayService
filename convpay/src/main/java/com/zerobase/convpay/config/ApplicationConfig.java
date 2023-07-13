1fc0246b528ed92393440cb5e5a59caea228c7ab
HeaSeongYoon <yoonhs989898@gmail.com>
2023-07-13 22:24:48 +0900

    스프링으로 전환. 프로젝트 완료

--- convpay/src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ convpay/src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,25 +1,45 @@
 package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+
+@Configuration
 public class ApplicationConfig {
 
-    public ConveniencePayService conveniencePayServiceDiscountConvenience(){
+    @Bean
+    public ConveniencePayService conveniencePayService(){
         return new ConveniencePayService(
                 new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
+                        Arrays.asList(moneyAdapter(), cardAdapter())
                 ),
-                new DiscountByConvenience()
+                discountByConvenience()
         );
+    }
+
+    @Bean
+    private static CardAdapter cardAdapter() {
+        return new CardAdapter();
+    }
+
+    @Bean
+    private static MoneyAdapter moneyAdapter() {
+        return new MoneyAdapter();
+    }
+
+    @Bean
+    public  DiscountByConvenience discountByConvenience() {
+        return new DiscountByConvenience();
     }
 
     public ConveniencePayService conveniencePayServiceDiscountPayMethod(){
         return new ConveniencePayService(
                 new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
+                        Arrays.asList(new MoneyAdapter(), cardAdapter())
                 ),
                 new DiscountByPayMethod()
         );
